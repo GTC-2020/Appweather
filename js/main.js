ccc2af30744784779c1fb422d25a99e680711755
George <tsurkan.g@gmail.com>
2022-03-10 14:17:42 +0200

    First Commit

---
+++ js/main.js
@@ -0,0 +1,56 @@
+import { UI_ELEMENTS } from "./view.js";
+import { getCityName } from "./view.js";
+import { showTemperature } from "./view.js";
+import { showName } from "./view.js";
+import { shoImageWeatherState } from "./view.js";
+import { addCityLocation } from "./view.js";
+import { deleteCity } from "./view.js";
+import { showSunSetRise } from "./view.js";
+
+const API_KEY = 'f660a2fb1e4bad108d6160b7f58c555f';
+export const SERVER_URL = 'http://api.openweathermap.org/data/2.5/weather';
+
+export function createUrl(cityName){
+    return `${SERVER_URL}?q=${cityName}&appid=${API_KEY}`;
+}
+
+export function getDate(url){
+    return fetch(url)
+    .then(response => response.json())
+}
+
+export function correctHourByTiemZone(hourUTC, timezone){
+    let h = hourUTC + timezone / 3600;
+    if(h > 24){
+        return '0'+(h-24);
+    }
+    return h;
+}
+
+
+
+
+UI_ELEMENTS.BUTTON_SEARCH.addEventListener('click',function(){
+    let cityName = getCityName();
+    showName(cityName);
+    showTemperature(cityName);
+    shoImageWeatherState(cityName);
+    deleteCity();
+    showSunSetRise(cityName);
+    UI_ELEMENTS.INPUT_CITY.value = '';
+})
+
+UI_ELEMENTS.BUTTON_ADD_CITY.addEventListener('click', function(){
+    addCityLocation();
+})
+
+let arr = UI_ELEMENTS.ADDED_CITY_NAME.querySelectorAll('.city_name');
+for(let key of arr){
+    key.addEventListener('click',function(){
+        showName(key.textContent);
+        showTemperature(key.textContent);
+        shoImageWeatherState(key.textContent);
+        showSunSetRise(key.textContent);
+        deleteCity()
+    })
+}
---
+++ js/view.js
@@ -0,0 +1,102 @@
+import { getDate } from "./main.js";
+import { createUrl } from "./main.js";
+import { correctHourByTiemZone } from "./main.js";
+
+export const UI_ELEMENTS={
+    INPUT_CITY:document.querySelector('#city_input'),
+    BUTTON_SEARCH:document.querySelector('.search'),
+    FIELD_NOW_CITY_NAME:document.querySelector('.fiels_name_city').querySelector('span'),
+    FIELD_NOW_TEMPERATURE:document.querySelector('.field_grade').querySelector('span'),
+    FIELD_NOW_IMAGE_STATE_WEATHER: document.querySelector('.fiels_state_weather').querySelector('img'),
+    BUTTON_ADD_CITY:document.querySelector('#shape'),
+    LIST_OF_CITY:document.querySelector('.added_city').querySelector('ul'),
+    ADDED_CITY_NAME:document.querySelector('.added_city'),
+    DETAILS_NAME_CITY:document.querySelector('.name-city').querySelector('p'),
+    DETAILS_TEMPERATURE_FIELD:document.querySelector('.temperatuer'),
+    DETAILE_FEELS_LIKE:document.querySelector('.feels_like'),
+    DETAIL_WEATHER:document.querySelector('.weather'),
+    DETAILS_SUNRISE:document.querySelector('.sunrise'),
+    DETAILS_SUNSET:document.querySelector('.sunset'),
+
+}
+
+const ADD_ELEMENT_LIST_CITY =  '<div class="city_name"></div><div class="btn_delete">';
+
+export function getCityName(){
+    return UI_ELEMENTS.INPUT_CITY.value;
+}
+
+export function showName(cityName){
+    getDate(createUrl(cityName))
+     .then(respone => {
+         UI_ELEMENTS.FIELD_NOW_CITY_NAME.textContent = respone.name;
+         UI_ELEMENTS.DETAILS_NAME_CITY.textContent = respone.name;
+     })
+     .catch(err => alert('Введите им города'));
+ }
+
+export function  showTemperature(cityName){
+    getDate(createUrl(cityName))
+    .then(response => {
+        UI_ELEMENTS.FIELD_NOW_TEMPERATURE.textContent = Math.floor(response.main.temp - 273.15);
+        UI_ELEMENTS.DETAILS_TEMPERATURE_FIELD.textContent =  Math.floor(response.main.temp - 273.15);
+        UI_ELEMENTS.DETAILE_FEELS_LIKE.textContent = Math.floor(response.main.feels_like - 273.15)
+    })
+    .catch(err => alert('Введите им города'));
+  
+}
+
+export function showSunSetRise(cityName){
+    getDate(createUrl(cityName))
+    .then(response => {
+        let timeSunrise = response.sys.sunrise;
+        let timeSunSet = response.sys.sunset;
+        let dateRise = new Date(timeSunrise * 1000);
+        let dateSet = new Date(timeSunSet * 1000);
+        let timezone =response.timezone;
+        let hourUTCRise = dateRise.getUTCHours();
+        let hourUTCSet = dateSet.getUTCHours();
+        let hourRise = correctHourByTiemZone(hourUTCRise, timezone);
+        let hourSet = correctHourByTiemZone(hourUTCSet, timezone);
+        UI_ELEMENTS.DETAILS_SUNRISE.textContent = `${hourRise}:${dateRise.getUTCMinutes()}`;
+        UI_ELEMENTS.DETAILS_SUNSET.textContent = `${hourSet}:${dateSet.getUTCMinutes()}`;
+        
+    })
+    
+}
+
+ export function shoImageWeatherState(cityName){
+    getDate(createUrl(cityName))
+    .then(respone =>{
+        UI_ELEMENTS.DETAIL_WEATHER.textContent = respone.weather[0].main;
+        UI_ELEMENTS.FIELD_NOW_IMAGE_STATE_WEATHER.src = `http://openweathermap.org/img/wn/${respone.weather[0].icon}@4x.png`;
+        
+    })
+    
+     
+ }
+
+export function addCityLocation(){
+    let addCity = document.createElement('li');
+    addCity.innerHTML = ADD_ELEMENT_LIST_CITY;
+    addCity.querySelector('.city_name').textContent = UI_ELEMENTS.FIELD_NOW_CITY_NAME.textContent;
+    UI_ELEMENTS.LIST_OF_CITY.append(addCity);
+
+    addCity.querySelector('.city_name').addEventListener('click', function(){
+        showName(addCity.textContent);
+        showTemperature(addCity.textContent);
+        shoImageWeatherState(addCity.textContent);
+        showSunSetRise(addCity.textContent);
+        deleteCity();
+    })
+}
+function deleteTask(event){
+    event.currentTarget.parentNode.remove();
+}
+
+export function deleteCity(){
+    let btnDelete = document.querySelectorAll('.btn_delete');
+    for(let btn of btnDelete){
+        btn.addEventListener('click', deleteTask);
+    }
+}
